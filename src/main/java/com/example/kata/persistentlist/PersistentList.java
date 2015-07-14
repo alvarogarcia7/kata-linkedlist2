2ec1aed6fe7e9e8f2b26654bd74585509ac61f76
Alvaro Garcia+Marc Manzano <derecerca@gmail.com>
2015-07-14 18:50:19 +0200

    red: should have size 1 when passing one element to the constructor

--- src/main/java/com/example/kata/persistentlist/PersistentList.java
+++ src/main/java/com/example/kata/persistentlist/PersistentList.java
@@ -1,7 +1,18 @@
 package com.example.kata.persistentlist;
 
 public class PersistentList {
+    public PersistentList(int i) {
+    }
+
+    public PersistentList() {
+
+    }
+
     public boolean isEmpty() {
         return true;
+    }
+
+    public int size() {
+        return 0;
     }
 }
--- src/test/java/ListShould.java
+++ src/test/java/ListShould.java
@@ -9,5 +9,11 @@ public class ListShould {
     @Test
     public void be_empty_by_default() {
         assertThat(new PersistentList().isEmpty(), is(true));
+
+    }
+
+    @Test
+    public void have_one_element_when_passing_it_to_the_constructor(){
+        assertThat(new PersistentList(1).size(),is(1));
     }
 }
