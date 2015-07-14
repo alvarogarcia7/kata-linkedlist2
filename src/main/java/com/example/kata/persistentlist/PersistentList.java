2e747d44b9078e073307fd88d87531bdaf15f6c0
Alvaro Garcia+Marc Manzano <derecerca@gmail.com>
2015-07-14 19:02:31 +0200

    refactor: DRY - unify all constructors that accept values in a single one

--- src/main/java/com/example/kata/persistentlist/PersistentList.java
+++ src/main/java/com/example/kata/persistentlist/PersistentList.java
@@ -4,20 +4,12 @@ public class PersistentList {
 
     int[] list;
 
-    public PersistentList(int i) {
-
-        list = new int[1];
-        list[0]=i;
-    }
-
     public PersistentList() {
 
     }
 
-    public PersistentList(int a, int b) {
-        list = new int[2];
-        list[0]=a;
-        list[1]=b;
+    public PersistentList(int...values) {
+        list = values;
     }
 
     public boolean isEmpty() {
