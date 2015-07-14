b3f4f3c1f03a9adac38dbc3232451e5b89606cb6
Alvaro Garcia+Marc Manzano <derecerca@gmail.com>
2015-07-14 18:41:26 +0200

    red: a list should be empty by default

---
+++ src/main/java/com/example/kata/persistentlist/PersistentList.java
@@ -0,0 +1,7 @@
+package com.example.kata.persistentlist;
+
+public class PersistentList {
+    public boolean isEmpty() {
+        return false;
+    }
+}
---
+++ src/test/java/ListShould.java
@@ -0,0 +1,14 @@
+import com.example.kata.persistentlist.PersistentList;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ListShould {
+
+    @Test
+    public void be_empty_by_default() {
+        assertThat(new PersistentList().isEmpty(), is(true));
+
+    }
+}
